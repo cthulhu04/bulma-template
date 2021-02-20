c8dd79eb632937414fc0cab761f729022aa955ee
diego bleecke <diego4@live.nl>
2021-02-20 18:50:04 +0100

    project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'./src')));
+app.use(express.static(path.join(__dirname,'./public')));
 
 app.get('/', (req, res, next) => {
     res.render('index');
